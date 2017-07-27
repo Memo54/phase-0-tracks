c0c240c88a1c16d94e599c18d06bbb0aab8acd13
Guillermo Soto <guillermo606r@gmail.com>
2017-07-27 00:36:26 -0500

    added ice cream with flavors toppings and how to mix them. also add arrays and hashes at the bottom with .keep_if .shift and other commands. Also something was wrong with my first commit that it shows 2 different iteration.rb but their both the same document

--- iteration.rb
+++ iteration.rb
@@ -42,3 +42,30 @@ p condiments
 condiments.each do |toppings, location|
   puts "#{toppings} are supposed to be #{location}"
 end
+#
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+ p numbers.select { |num| num.even? }
+
+ p numbers.reject { |num| num.even? }
+
+ p numbers.take_while { |i| i < 7 }
+
+ p numbers.drop_while { |i| i <= 6 }
+
+ p numbers.bsearch { |i| i >= 5 }
+
+#
+
+letters = {"a" => 10, "b" => 25, "c" => 37, "d" => 40, "e" => 55, "f" => 69}
+
+  p letters.keep_if { |letter, value| value >= 10 }
+
+  p letters.shift
+  p letters
+
+  d = "d"
+  p letters.key(37).equal? d
+
+  p letters.delete_if { |letter, value| value >= 40}
