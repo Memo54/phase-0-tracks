5ac93aaad3980cb72fe23763c4d27805d9299a5d
Guillermo Soto <guillermo606r@gmail.com>
2017-08-04 15:12:34 -0500

    nothing

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -1,4 +1,4 @@
-# Array Drills
+ # Array Drills
 
 zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
                               "shotgun", "compass", "CB radio", "batteries"]
