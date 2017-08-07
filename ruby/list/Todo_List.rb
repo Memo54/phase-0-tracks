3cea342c0f72f3ef2cd5a2fe52cabb873f9df29e
Guillermo Soto <guillermo606r@gmail.com>
2017-08-07 09:03:32 -0500

    all of the 4 test passed

---
+++ ruby/list/Todo_List.rb
@@ -0,0 +1,29 @@
+# class TodoList
+#   def get_items(x,y)
+#   todolist = [x,y]
+#   end
+
+# end
+
+# def get_items(x,y)
+#   [x,y]
+# end
+# p get_items("do the dishes", "mow the lawn")
+
+class TodoList
+  def initialize(list)
+    @list = list
+  end
+  def get_items
+    @list
+  end
+  def add_item(z)
+  @list = @list << z
+  end
+  def delete_item(w)
+  @list.delete(w)
+  end
+  def get_item(index)
+    @list.at(index)
+  end
+end
