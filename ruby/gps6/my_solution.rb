7a92f2326fa7a4384993be54a14357b54cbdb5f0
Guillermo Soto <guillermo606r@gmail.com>
2017-08-14 01:50:13 -0500

    state report for each 50 states.

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -4,25 +4,26 @@
 # We spent [#] hours on this challenge.
 
 # EXPLANATION OF require_relative
-#
+# it requires file information from an file adjacent to current file
 #
 require_relative 'state_data'
 
 class VirusPredictor
-
+# Initialize creates different instance for every state
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
-
+# Virus_effects its showing predicting deaths and speed of spread.
   def virus_effects
     predicted_deaths(@population_density, @population, @state)
     speed_of_spread(@population_density, @state)
   end
 
   private
-
+  #Method takes 3 variables from state data's hash. Then it runs if, elsif and else statements of how many people are killed by the disease by rounding up the interger.
+  #The data is printed in readable format
   def predicted_deaths(population_density, population, state)
     # predicted deaths is solely based on population density
     if @population_density >= 200
@@ -40,7 +41,7 @@ class VirusPredictor
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
-
+  #This method takes only 2 variables, as well an if/ elsif to see how fast the disease spreads.
   def speed_of_spread(population_density, state) #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
@@ -63,12 +64,19 @@ class VirusPredictor
   end
 
 end
+  #This method has to be outside of the the class so that all the data has a method instead of having an instance for every state.
 
+  def state_report
+  STATE_DATA.each do |states, population_density, population|
+  states = VirusPredictor.new(states, STATE_DATA[states][:population_density], STATE_DATA[states][:population])
+  states.virus_effects
+  end
+end
 #=======================================================================
 
 # DRIVER CODE
  # initialize VirusPredictor for each state
-
+#iterate throught the state_data hash, replace 4 states and do all 50.
 
 alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
 alabama.virus_effects
@@ -82,6 +90,10 @@ california.virus_effects
 alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
 alaska.virus_effects
 
-
+state_report
 #=======================================================================
 # Reflection Section
+
+# What two different hash syntaxes are in the state_data?
+# Could a rocket to set a key?
+# How do you iterate through a hash?
